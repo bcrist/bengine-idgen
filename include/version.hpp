f8a9dbd406b150ac2f3955d6a37900e3ce30cb46
Ben Crist <ben@magicmoremagic.com>
2017-04-09 18:17:48 -0500

    versioning

---
+++ include/version.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef BE_IDGEN_VERSION_HPP_
+#define BE_IDGEN_VERSION_HPP_
+
+#include <be/core/macros.hpp>
+
+#define BE_IDGEN_VERSION_MAJOR 0
+#define BE_IDGEN_VERSION_MINOR 1
+#define BE_IDGEN_VERSION_REV 1
+
+/*!! include('common/version', 'BE_IDGEN', 'idgen') !! 6 */
+/* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
+#define BE_IDGEN_VERSION (BE_IDGEN_VERSION_MAJOR * 100000 + BE_IDGEN_VERSION_MINOR * 1000 + BE_IDGEN_VERSION_REV)
+#define BE_IDGEN_VERSION_STRING "idgen " BE_STRINGIFY(BE_IDGEN_VERSION_MAJOR) "." BE_STRINGIFY(BE_IDGEN_VERSION_MINOR) "." BE_STRINGIFY(BE_IDGEN_VERSION_REV)
+
+/* ######################### END OF GENERATED CODE ######################### */
+
+#endif
